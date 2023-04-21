6e0460ba2274ef451a5873eb55d4b44c8c57b13e
Мария Хамицкая <mariakand_01@mail.ru>
2023-04-21 15:03:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var numberOne=2, string='Hello', snow=false, empty=null;
+var typenumber='number', typestring='string', typebooling='booling', typenull='null', question;
+alert("Добро пожаловать");
+alert("У меня есть 4 переменных с разными типами");
+alert("Тут значение переменной. " +numberOne);
+alert("Это тип переменной. " +typenumber);
+
+alert("Тут значение перменной. " +string);
+alert("Это тип переменной. " +typestring);
+
+alert("Тут значение перменной. " +snow);
+alert("Это тип переменной. " +typebooling);
+
+alert("Тут значение перменной. " +empty);
+alert("Это тип переменной. " +typenull);
+
+question=prompt("Понравился ли вам сайт?");
+alert(" " +question);
